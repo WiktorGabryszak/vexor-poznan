14157342df2dcf0f7dcf4fdcc90c9ee13141d1f7
WiktorGabryszak <gabryszakwg@gmail.com>
2026-05-24 23:15:57 +0200

    added domain stronyinternetowe.net

--- src/pages/sitemap.xml.ts
+++ src/pages/sitemap.xml.ts
@@ -12,7 +12,7 @@ export const GET: APIRoute = () => {
     { url: '/projekty/', priority: '0.8', changefreq: 'weekly' },
     { url: '/kontakt/', priority: '0.9', changefreq: 'monthly' },
   ];
-  const baseUrl = 'https://vexordesigns.pl';
+  const baseUrl = 'https://stronyinternetowe.net';
   const today = new Date().toISOString().split('T')[0];
   const xml = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
