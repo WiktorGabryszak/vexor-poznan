8cacba0e9acb146fa4d73be02de8aba6bceeb463
Wiktor Macbook <gabryszakwg@gmail.com>
2026-05-25 09:32:59 +0200

    Privacy / Cookies Policies, fixed internal links, fixed @2025 to @2026, added baner cookie

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,10 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://stronyinternetowe.net',
+	redirects: {
+		'/uslugi/strony-internetowe/': '/uslugi/projektowanie-stron-internetowych/',
+		'/uslugi/sklepy-internetowe/': '/uslugi/tworzenie-sklepow-internetowych/',
+	},
 	integrations: [tailwind({ applyBaseStyles: false })],
 	vite: {
 		optimizeDeps: {
